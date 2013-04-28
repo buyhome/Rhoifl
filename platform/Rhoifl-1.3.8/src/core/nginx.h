aef03c744f8e020c3e81f97ea9a655c147a416bc
buyhome <pai6@qq.com>
2013-04-28 17:03:12 +0800

    init.d

---
+++ platform/Rhoifl-1.3.8/src/core/nginx.h
@@ -0,0 +1,20 @@
+
+/*
+ * Copyright (C) Igor Sysoev
+ * Copyright (C) Nginx, Inc.
+ */
+
+
+#ifndef _NGINX_H_INCLUDED_
+#define _NGINX_H_INCLUDED_
+
+
+#define nginx_version      1003008
+#define NGINX_VERSION      "1.3.8"
+#define NGINX_VER          "Rhoifl/" NGINX_VERSION
+
+#define NGINX_VAR          "RHOIFL"
+#define NGX_OLDPID_EXT     ".oldbin"
+
+
+#endif /* _NGINX_H_INCLUDED_ */
